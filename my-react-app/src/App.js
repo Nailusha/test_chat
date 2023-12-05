67172faf0d267b9ee6fde20adcd611d205844e3e
Kazachuk Nailya <yakimovanailya@yandex.ru>
2023-12-05 13:05:32 +0300

    commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SimpleChatBot from './SimpleChatBot';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <SimpleChatBot />
+      </main>
+    </div>
+  );
+}
+
+export default App;
